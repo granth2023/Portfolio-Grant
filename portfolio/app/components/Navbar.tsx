ccc1d84f51eea6c629e390f9d01eb608398df5d7
granth2023 <grantharris38@gmail.com>
2023-08-11 00:09:32 -0400

    establish function

--- portfolio/app/components/Navbar.tsx
+++ portfolio/app/components/Navbar.tsx
@@ -4,3 +4,9 @@ import { Disclosure } from "@headlessui/react";
 import Link from "next/link";
 import  { usePathname } from "next/navigation";
 import Themebutton from "./Themebutton";
+
+export default function Navbar() {
+
+
+    
+}
