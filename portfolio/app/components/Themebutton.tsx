e96cc45e9ca1cbb9131d285768fa71e11d9c7700
granth2023 <grantharris38@gmail.com>
2023-08-11 00:36:37 -0400

    function added

--- portfolio/app/components/Themebutton.tsx
+++ portfolio/app/components/Themebutton.tsx
@@ -6,4 +6,13 @@ import { useEffect, useState } from "react";
 export default function Themebutton() {
     const { setTheme, resolvedTheme } = useTheme();
     const [mounted, setMounted] = useState(false);
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if(!mounted) {
+        return null;
 }
+}
+    
