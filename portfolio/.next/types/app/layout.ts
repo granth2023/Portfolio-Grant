fcabb11ef7bc26c2d0fd90da4364334161bd2853
granth2023 <grantharris38@gmail.com>
2023-08-11 04:46:06 -0400

    layout

--- portfolio/.next/types/app/layout.ts
+++ portfolio/.next/types/app/layout.ts
@@ -1,8 +1,8 @@
 // File: /Users/gsh/code/next/portfolio/nextjs_portoflio/portfolio/app/layout.tsx
-import * as entry from '../../../app/layout.js'
+import * as entry from '../../../../app/layout.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../app/layout.js')
+type TEntry = typeof import('../../../../app/layout.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
--- portfolio/.next/types/app/page.ts
+++ portfolio/.next/types/app/page.ts
@@ -1,8 +1,8 @@
 // File: /Users/gsh/code/next/portfolio/nextjs_portoflio/portfolio/app/page.tsx
-import * as entry from '../../../app/page.js'
+import * as entry from '../../../../app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
-type TEntry = typeof import('../../../app/page.js')
+type TEntry = typeof import('../../../../app/page.js')
 
 // Check that the entry is a valid entry
 checkFields<Diff<{
