b8edafa6d60d93ff6c4f6a4c594ec6cac22ee2a3
granth2023 <grantharris38@gmail.com>
2023-08-11 00:35:48 -0400

    create theme button

---
+++ portfolio/app/components/Themebutton.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+export default function Themebutton() {
+    const { setTheme, resolvedTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+}
