172c9c838108cd0791798ce818c78e3d0d218fd0
granth2023 <grantharris38@gmail.com>
2023-08-11 00:14:12 -0400

    name added with styling

--- portfolio/app/components/Navbar.tsx
+++ portfolio/app/components/Navbar.tsx
@@ -6,7 +6,35 @@ import  { usePathname } from "next/navigation";
 import Themebutton from "./Themebutton";
 
 export default function Navbar() {
+    let pathname = usePathname() || "/";
+    return (
+        <DisclosureContainer as "nav">
+            {({ open }) => (
+                <>
+                <div className="max-w6xl mx-auto px-4 sm:px-6 lg:px-8">
+                    <div className="flex jsutify-between h-16">
+                        <div className="flex justify-between w-full">
+                            <div className="flex items-center">
+                                <Link href="/">
+                                    <h1 className="text-2xl font medium"> 
+                                        Grant <span span className="text-teal-5000"> Harris</span>
+                                        </h1>
+
+                                </Link>
+                                <Themebutton/>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                
+                </>
+            )}
 
 
-    
+        </DisclosureContainer>
+    )
+
+
+
 }
