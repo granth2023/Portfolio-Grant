5320ff82eab65d604fad8cb3288ea3fedd53c0ce
granth2023 <grantharris38@gmail.com>
2023-08-11 00:50:22 -0400

    action routed

--- portfolio/app/action.ts
+++ portfolio/app/action.ts
@@ -1,1 +1,18 @@
 "use server"; 
+
+import { revalidatePath } from "next/cache"; 
+import { prisma } from "./db";
+
+export async function postEntry(formData: FormData) {
+    "use server";
+
+    const data = await prisma.guestbook.create({
+        data : { 
+            message: formData.get("entry") as string,
+            username: "hello",
+        },
+    });
+
+    revalidatePath("/guestbook");
+
+}
