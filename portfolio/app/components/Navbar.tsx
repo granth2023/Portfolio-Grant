e55ab872145fbe75ed826555ccc6c386e44a865b
granth2023 <grantharris38@gmail.com>
2023-08-11 00:29:05 -0400

    icons added

--- portfolio/app/components/Navbar.tsx
+++ portfolio/app/components/Navbar.tsx
@@ -50,10 +50,43 @@ export default function Navbar() {
                                 <Themebutton/>
                             </div>
                         </div>
-                        <
+                        <div className="-mr-2 flex items-center sm:hidden">
+                            <Themebutton />
+                            <Disclosure.Button className="inline-flex items-center jsutify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus: ring-teal-500 dark:hover:bg-gray-800">
+                            { open ?}(
+                                <svg 
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    file="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="w-6 h-6"
+                                    > 
+                                    <path 
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="m6 18L18 6m6 6l12 12"
+                                        />
+                                    </svg>
+                            ) : ( 
+                                <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth={1.5}
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+                                />
+                              </svg>
+                            )}
+                          </Disclosure.Button>  
+                        </div>
                     </div>
-
-
                 </div>
                 
                 </>
