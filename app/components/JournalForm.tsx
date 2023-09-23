f8bb4e9cfb6ba405cb0406889c5bc1aa505c250f
granth2023 <grantharris38@gmail.com>
2023-09-23 12:26:12 -0400

    got rid of duplicate function

--- app/components/JournalForm.tsx
+++ app/components/JournalForm.tsx
@@ -1,49 +1,49 @@
-import Head from 'next/head';
-import sanityClient from '../../sanityClient';
-import React, { useState, useEffect } from 'react';
+// import Head from 'next/head';
+// import sanityClient from '../../sanityClient';
+// import React, { useState, useEffect } from 'react';
 
-interface JournalEntry {
-  _id: string;
-  title: string;
-  content: string;
-  date: string;
-}
+// interface JournalEntry {
+//   _id: string;
+//   title: string;
+//   content: string;
+//   date: string;
+// }
 
-function LiveJournal() {
-  const [entries, setEntries] = useState<JournalEntry[]>([]);
+// function LiveJournal() {
+//   const [entries, setEntries] = useState<JournalEntry[]>([]);
 
-  useEffect(() => {
-    const fetchEntries = async () => {
-      const query = `*[_type == "journalEntry"] | order(date desc) {
-        _id,
-        title,
-        content,
-        date,
-      }`;
+//   useEffect(() => {
+//     const fetchEntries = async () => {
+//       const query = `*[_type == "journalEntry"] | order(date desc) {
+//         _id,
+//         title,
+//         content,
+//         date,
+//       }`;
 
-      const fetchedEntries: JournalEntry[] = await sanityClient.fetch(query);
-      setEntries(fetchedEntries || []);
-    }
+//       const fetchedEntries: JournalEntry[] = await sanityClient.fetch(query);
+//       setEntries(fetchedEntries || []);
+//     }
 
-    fetchEntries();
-  }, []);
+//     fetchEntries();
+//   }, []);
 
-  return (
-    <div className="container mx-auto px-4 mt-6">
-      <Head>
-        <title>Live Journal - Grant Harris</title>
-      </Head>
-      <h1 className="text-3xl mb-6 font-bold text-center text-gray-700">Live Journal</h1>
-      <section className="mt-10">
-        {entries && entries.map((entry: JournalEntry) => (
-          <div key={entry._id} className="mb-6 p-4 rounded shadow bg-white" style={{ color: 'black', backgroundColor: 'white' }}>
-            <p className="text-black">{entry.content}</p>
-            <small className="text-gray-600">{new Date(entry.date).toLocaleDateString()}</small>
-          </div>
-        ))}
-      </section>
-    </div>
-  );
-}
+//   return (
+//     <div className="container mx-auto px-4 mt-6">
+//       <Head>
+//         <title>Live Journal - Grant Harris</title>
+//       </Head>
+//       <h1 className="text-3xl mb-6 font-bold text-center text-gray-700">Live Journal</h1>
+//       <section className="mt-10">
+//         {entries && entries.map((entry: JournalEntry) => (
+//           <div key={entry._id} className="mb-6 p-4 rounded shadow bg-white" style={{ color: 'black', backgroundColor: 'white' }}>
+//             <p className="text-black">{entry.content}</p>
+//             <small className="text-gray-600">{new Date(entry.date).toLocaleDateString()}</small>
+//           </div>
+//         ))}
+//       </section>
+//     </div>
+//   );
+// }
 
-export default LiveJournal;
+// export default LiveJournal;
--- app/livejournal/page.tsx
+++ app/livejournal/page.tsx
@@ -2,7 +2,7 @@
 
 import Head from 'next/head';
 import sanityClient from '../../sanityClient';
-import JournalForm from '../components/JournalForm';
+// import JournalForm from '../components/JournalForm';
 import React, { useState, useEffect } from 'react';
 
 interface JournalEntry {
@@ -37,7 +37,7 @@ function LiveJournal() {
         <title>Live Journal - Grant Harris</title>
       </Head>
       <h1 className="text-3xl mb-6 font-bold text-center text-gray-700"></h1>
-      <JournalForm />
+      {/* <JournalForm /> */}
       <section className="mt-10">
         {entries && entries.map((entry: JournalEntry) => (
           <div key={entry._id} className="mb-6 p-4 rounded shadow bg-white" style={{ color: 'black', backgroundColor: 'white' }}>
