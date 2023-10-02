164f1e77c2cd74d1736a5a5437ddbbe820176e22
granth2023 <grantharris38@gmail.com>
2023-10-02 10:37:21 -0400

    linkedlists merge

--- app/livejournal/code.js
+++ app/livejournal/code.js
@@ -32,3 +32,35 @@
 
 //     return x === revertedNumber || x === Math.floor(revertedNumber / 10);
 // }
+
+// Linked Lists 
+
+
+// function ListNode(val, next) {
+//     this.val = (val === undefined ? 0 : val);
+//     this.next = (next === undefined ? null : next);
+// }
+    //    var mergeTwoLists = function(l1, l2) {
+    //     let dummy = new ListNode();
+    //     let current = dummy; 
+
+    //     while (l1 !== null && l2 !== null) {
+    //         if (l1.val < l2.val) {
+    //             current.next = l1; 
+    //             l1 = l1.next; 
+    //         } else {
+    //             current.next = l2; 
+    //             l2 = l2.next; 
+    //         }
+    //         current = current.next; 
+    //     }
+
+    //     if (l1 !== null) {
+    //         current.next =l1;
+    //     } else  {
+    //         current.next = l2; 
+
+    //     }
+
+    //     return dummy.next
+    //     //
