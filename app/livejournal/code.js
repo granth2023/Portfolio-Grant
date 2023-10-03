814d4e96ddfa0145c29f09c788639627008a4bb1
granth2023 <grantharris38@gmail.com>
2023-10-03 11:48:21 -0400

    commented out needleinhaystack

--- app/livejournal/code.js
+++ app/livejournal/code.js
@@ -170,15 +170,15 @@
     //         }
         
 
-    function findNeedle(haystack, needle){
-        if (needle =""){
-            return 0;
-        }
-        for(let i =0; i < haystack.length - needle.length; i++){
-            if (haystack.substring(i, i + needle.length) === needle){
-                return i;
-            }
-            return -1;
-    }
-}
+//     function findNeedle(haystack, needle){
+//         if (needle =""){
+//             return 0;
+//         }
+//         for(let i =0; i < haystack.length - needle.length; i++){
+//             if (haystack.substring(i, i + needle.length) === needle){
+//                 return i;
+//             }
+//             return -1;
+//     }
+// }
     
