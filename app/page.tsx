1ff5fd43c6fbac5f7493b06f6ed4131ff3bfeef7
granth2023 <grantharris38@gmail.com>
2023-10-10 17:08:14 -0400

    pdf troubleshoot

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
           <p className="text-gray-500 dark:text-gray-300 text-center">
             Click
             <a
-               href="../public/SDResume.pdf"
+               href="public/SDResume.pdf"
               target="_blank"
             >
               {" "}
