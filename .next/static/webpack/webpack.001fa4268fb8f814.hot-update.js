9e759a2a327124eeba615b204e15f00031fe1cca
granth2023 <grantharris38@gmail.com>
2023-11-01 15:16:18 -0400

    commit

--- .next/static/webpack/webpack.001fa4268fb8f814.hot-update.js
+++ .next/static/webpack/webpack.001fa4268fb8f814.hot-update.js
@@ -9,9 +9,18 @@
  */
 self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
+/******/ /* webpack/runtime/get javascript chunk filename */
+/******/ !function() {
+/******/ 	// This function allow to reference async chunks
+/******/ 	__webpack_require__.u = function(chunkId) {
+/******/ 		// return url for filenames based on template
+/******/ 		return "static/chunks/" + chunkId + ".js";
+/******/ 	};
+/******/ }();
+/******/ 
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "efc9b89b7cd47c4c"; }
+/******/ 	__webpack_require__.h = function() { return "7ec3f354020d9c81"; }
 /******/ }();
 /******/ 
 /******/ }
